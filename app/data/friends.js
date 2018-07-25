975f2082be2b804605d3cc2aa36ecb216ca7ccb2
george <geomarray@gmail.com>
2018-07-25 14:00:23 -0700

    almost working app

---
+++ app/data/friends.js
@@ -0,0 +1,50 @@
+var friendsArray = [
+    {
+        name:  "Peewee Herman",
+        photo : "https://i.imgflip.com/10nkit.jpg",
+        scores:[
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    }, {
+        name:"Alice Cooper",
+        photo:"http://www.alternativenation.net/wp-content/uploads/2018/02/alicecooperhands.jpg",
+        scores:[
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    } , {
+        name:"Bob Barker",
+        photo: "https://www.golfchannel.com/sites/golfchannel.prod.acquia-sites.com/files/styles/gallery_slide/public/bobbarker7.jpg?itok=MYIkQWNM",
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    }
+]
+
+module.exports = friendsArray;
